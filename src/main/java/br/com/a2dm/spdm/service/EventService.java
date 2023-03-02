a019bce39f4c9b23c8b4531e9e64318892023367
jpedropmont <jpedropmont@gmail.com>
2023-03-02 01:27:09 -0300

    Adicionando ações de aprovar e reprovar evento

--- src/main/java/br/com/a2dm/spdm/service/EventService.java
+++ src/main/java/br/com/a2dm/spdm/service/EventService.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.hibernate.Criteria;
+import org.hibernate.FlushMode;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 import org.hibernate.sql.JoinType;
 
 import br.com.a2dm.brcmn.entity.ativmob.Event;
 import br.com.a2dm.brcmn.util.A2DMHbNgc;
+import br.com.a2dm.brcmn.util.HibernateUtil;
 import br.com.a2dm.brcmn.util.RestritorHb;
 
 public class EventService extends A2DMHbNgc<Event>
@@ -51,6 +54,66 @@ public class EventService extends A2DMHbNgc<Event>
 	    }
 		
 		return criteria;
+	}
+	
+	public Event aprovar(Event vo) throws Exception
+	{
+		Session sessao = HibernateUtil.getSession();
+		sessao.setFlushMode(FlushMode.COMMIT);
+		Transaction tx = sessao.beginTransaction();
+		try
+		{
+			vo = aprovar(sessao, vo);
+			tx.commit();
+			return vo;
+		}
+		catch (Exception e)
+		{
+			tx.rollback();
+			throw e;
+		}
+		finally
+		{
+			sessao.close();
+		}
+	}
+	
+	public Event aprovar(Session sessao, Event vo) throws Exception
+	{	
+		vo.setStatus("Aprovado");
+		sessao.merge(vo);
+		
+		return vo;
+	}
+	
+	public Event reprovar(Event vo) throws Exception
+	{
+		Session sessao = HibernateUtil.getSession();
+		sessao.setFlushMode(FlushMode.COMMIT);
+		Transaction tx = sessao.beginTransaction();
+		try
+		{
+			vo = reprovar(sessao, vo);
+			tx.commit();
+			return vo;
+		}
+		catch (Exception e)
+		{
+			tx.rollback();
+			throw e;
+		}
+		finally
+		{
+			sessao.close();
+		}
+	}
+	
+	public Event reprovar(Session sessao, Event vo) throws Exception
+	{
+		vo.setStatus("Reprovado");
+		sessao.merge(vo);
+		
+		return vo;
 	}
 		
 	@Override
