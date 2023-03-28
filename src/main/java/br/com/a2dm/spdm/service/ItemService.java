92544eb7af195fb1777c42b9e15bc03ff400bd7c
Mateus Bastos <mateus@treinocertold.com.br>
2023-03-28 09:55:31 -0300

    Alterando quantidade do item

--- src/main/java/br/com/a2dm/spdm/service/ItemService.java
+++ src/main/java/br/com/a2dm/spdm/service/ItemService.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.hibernate.Criteria;
+import org.hibernate.FlushMode;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import br.com.a2dm.brcmn.util.A2DMHbNgc;
+import br.com.a2dm.brcmn.util.HibernateUtil;
 import br.com.a2dm.brcmn.util.RestritorHb;
 import br.com.a2dm.spdm.entity.Item;
 
@@ -34,6 +37,7 @@ public class ItemService extends A2DMHbNgc<Item>
 	
 	public ItemService()
 	{
+		adicionarFiltro("idItem", RestritorHb.RESTRITOR_EQ, "idItem");
 		adicionarFiltro("idSugestaoPedido", RestritorHb.RESTRITOR_EQ, "idSugestaoPedido");
 	}
 	
@@ -58,4 +62,39 @@ public class ItemService extends A2DMHbNgc<Item>
 	{
 		return filtroPropriedade;
 	}
+	
+	public Item atualizarQuantidade(Item vo) throws Exception
+	{
+		Session sessao = HibernateUtil.getSession();
+		sessao.setFlushMode(FlushMode.COMMIT);
+		Transaction tx = sessao.beginTransaction();
+		
+		try
+		{
+			vo = atualizarQuantidade(sessao, vo);
+			tx.commit();
+			return vo;
+		}
+		catch (Exception e)
+		{
+			tx.rollback();
+			throw e;
+		}
+		finally
+		{
+			sessao.close();
+		}
+	}
+
+	public Item atualizarQuantidade(Session sessao, Item item) throws Exception {
+		Item itemFind = new Item();
+		itemFind.setIdItem(item.getIdItem());
+		
+		itemFind = get(sessao, itemFind, 0);
+		
+		itemFind.setValue(item.getValue());
+		
+		sessao.merge(itemFind);
+		return itemFind;
+	}	
 }
